5b0c4f18430228718c49df7487288c021bd905d6
Afdal Habibur Rahman <afdalrahman459@gmail.com>
2026-04-15 23:15:36 +0700

    final bangett

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,3 +19,4 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 }
+    
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+    @extends('layouts.app')
 
 @section('title', 'Dashboard - DompetKu')
 @section('body-class', 'd-flex flex-column min-vh-100 bg-light')
